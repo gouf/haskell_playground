f81087c00e3c2377e8ccbf8d135999f212505167
gouf <innocent.zero@gmail.com>
2021-09-26 17:58:46 +0900

    refactor: 処理を関数に切り出し

--- first_prog.hs
+++ first_prog.hs
@@ -1,11 +1,22 @@
-messyMain :: IO ()
-messyMain = do
-  print "Who is the email for?"
-  recipient <- getLine
-  print "What is the Title?"
-  title <- getLine
-  print "Who is the Author?"
-  author <- getLine
-  print (
-    "Dear " ++ recipient ++ ",\n" ++
-    "Thanks for buing " ++ title ++ "\nthanks,\n" ++ author)
+toPart recipient = "Dear " ++ recipient ++ ",\n"
+bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ ".\n"
+fromPart author = "Thanks,\n" ++ author
+
+createEmail recipient bookTitle author = toPart recipient ++
+                                         bodyPart bookTitle ++
+                                         fromPart author
+ask message = do
+  print message
+  getLine
+
+askRecipient = ask "Who is the email for?"
+
+askTitle = ask "What is the Title?"
+
+askAuthor = ask "Who is the Author?"
+
+main = do
+  recipient <- askRecipient
+  title <- askTitle
+  author <- askAuthor
+  print (createEmail recipient title author)
